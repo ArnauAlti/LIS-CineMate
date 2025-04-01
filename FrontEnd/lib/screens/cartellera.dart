1cda3fa3cbb402f1a4faa27944c00f547d2fa0ac
Joan <115805319+NIU1667454@users.noreply.github.com>
2025-04-01 19:40:07 +0200

    Update pantalla cartellera

--- FrontEnd/lib/screens/cartellera.dart
+++ FrontEnd/lib/screens/cartellera.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'registre.dart';
+import 'inici_sessio.dart';
 
 class CartelleraScreen extends StatelessWidget {
   const CartelleraScreen({super.key});
@@ -7,11 +9,103 @@ class CartelleraScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[50],
-      appBar: AppBar(title: const Text("Biblioteca"), backgroundColor: Colors.black, foregroundColor: Colors.white),
-      body: const Center(
-        child: Text(
-          "Bienvenido a la Cartelera",
-          style: TextStyle(fontSize: 24),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        title: const Text("Cartellera", textAlign: TextAlign.center),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              // TODO: Acció al polsar la lupa
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Container(
+              height: kToolbarHeight, // Mateixa alçada que la AppBar
+              color: Colors.black,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(left: 16.0),
+                    child: Text(
+                      "Menú",
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.close, color: Colors.white),
+                    onPressed: () {
+                      Navigator.of(context).pop(); // Creu que tanca el Drawer (Menu desplegable)
+                    },
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              title: const Text("Registro"),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RegistreScreen()),
+                );
+                },
+            ),
+            ListTile(
+              title: const Text("Inicio de sesión"),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginScreen()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildMovieBox("Pelicula 1"),
+                _buildMovieBox("Pelicula 2"),
+              ],
+            ),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildMovieBox("Pelicula 3"),
+                _buildMovieBox("Pelicula 4"),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //Funció per crear les caixes per cada portada de sèrie i pel·lícula
+  Widget _buildMovieBox(String title) {
+    return Container(
+      width: 135,
+      height: 200,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black, width: 2),
+      ),
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(title),
         ),
       ),
     );
