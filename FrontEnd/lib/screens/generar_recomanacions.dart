8544ed3e01666079ba7f2f5f3c59e7cd41c16b1a
Joan <joan.corrals@autonoma.cat>
2025-04-07 14:16:54 +0200

    Pantalla de recomanacions feta

---
+++ FrontEnd/lib/screens/generar_recomanacions.dart
@@ -0,0 +1,114 @@
+import 'package:cine_mate/screens/detalls_peli_serie.dart';
+import 'package:flutter/material.dart';
+
+class RecomanacionsGeneradesScreen extends StatefulWidget {
+  final String? selectedGenre;
+
+  const RecomanacionsGeneradesScreen({super.key, this.selectedGenre});
+
+  @override
+  State<RecomanacionsGeneradesScreen> createState() => _RecomanacionsGenerades();
+}
+
+class _RecomanacionsGenerades extends State<RecomanacionsGeneradesScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.blue[50],
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        title: const Text("Resultados"),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 15),
+          const Text("Pensamos que estas películas y/o series te pueden gustar",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 30),
+
+          // Mostrar el género seleccionado en una línea
+          if (widget.selectedGenre != null)
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
+                "Género con mayor peso: ${widget.selectedGenre}",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+
+          const SizedBox(height: 50),
+          Expanded(
+            child: Column(
+              children: [
+                //TODO: Agafar pel·lícules i sèries a partir de la IA
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildMovieBox(context, "Película 1"),
+                    _buildMovieBox(context, "Película 2"),
+                  ],
+                ),
+                const SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildMovieBox(context, "Película 3"),
+                    _buildMovieBox(context, "Película 4"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMovieBox(BuildContext context, String title) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetallsPeliSerieScreen(title: title),
+          ),
+        );
+      },
+      child: Container(
+        width: 135,
+        height: 200,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 2),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(title, textAlign: TextAlign.center),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- FrontEnd/lib/screens/recomanacions.dart
+++ FrontEnd/lib/screens/recomanacions.dart
@@ -1,27 +1,60 @@
+import 'package:cine_mate/screens/generar_recomanacions.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../user_role_provider.dart';
+import 'app_drawer.dart';
 
-class RecomanacionsScreen extends StatelessWidget {
+class RecomanacionsScreen extends StatefulWidget {
   const RecomanacionsScreen({super.key});
 
   @override
+  _RecomanacionsScreenState createState() => _RecomanacionsScreenState();
+}
+
+class _RecomanacionsScreenState extends State<RecomanacionsScreen> {
+  String? _selectedGenre;  // Variable para almacenar la opción seleccionada
+
+  @override
   Widget build(BuildContext context) {
+    final userRoleProvider = Provider.of<UserRoleProvider>(context);
+    final userRole = userRoleProvider.userRole;
+
     return Scaffold(
       backgroundColor: Colors.blue[50],
       appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Colors.black,
         foregroundColor: Colors.white,
         title: const Text("Recomendaciones"),
+      ),
+      drawer: AppDrawer(
+        userRole: userRole,
+        onRoleChange: (String newRole) {
+          userRoleProvider.setUserRole(newRole);
+        },
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text("Generar recomendaciones a partir de..."),
+            const Text(
+              "Generar recomendaciones a partir de:",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             const SizedBox(height: 24),
             ElevatedButton(
               onPressed: () {
                 // TODO: Generar recomendaciones a partir del usuario mismo
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RecomanacionsGeneradesScreen(selectedGenre: _selectedGenre),
+                  ),
+                );
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.black,
@@ -37,6 +70,10 @@ class RecomanacionsScreen extends StatelessWidget {
             ElevatedButton(
               onPressed: () {
                 // TODO: Generar recomendaciones a partir de otros usuarios
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RecomanacionsGeneradesScreen()),
+                );
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.black,
@@ -48,6 +85,47 @@ class RecomanacionsScreen extends StatelessWidget {
               ),
               child: const Text("Usuarios que sigo"),
             ),
+            const SizedBox(height: 50),
+            const Text(
+              "Dar más peso al siguiente género:",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 16),
+            DropdownButton<String>(
+              alignment: Alignment.center,
+              value: _selectedGenre, // Asignamos el valor seleccionado
+              items: const [
+                DropdownMenuItem<String>(
+                  value: 'Terror',
+                  child: Text('Terror'),
+                ),
+                DropdownMenuItem<String>(
+                  value: 'Comedia',
+                  child: Text('Comedia'),
+                ),
+                DropdownMenuItem<String>(
+                  value: 'Drama',
+                  child: Text('Drama'),
+                ),
+              ],
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedGenre = newValue; // Actualizamos el valor seleccionado
+                });
+              },
+              hint: const Text('Selecciona un género'),
+              isExpanded: true,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+              ),
+              iconEnabledColor: Colors.black,
+              underline: const SizedBox(),
+            ),
+            const SizedBox(height: 32),
           ],
         ),
       ),
