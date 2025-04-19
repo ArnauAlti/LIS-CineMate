d58e75e12c3b5acdb71085a0c3fed7582443312b
MaikiNootNoot <msanbos@gmai.com>
2025-04-19 19:47:01 +0200

    added genres table and fix some issue

--- database-init/init.sql
+++ database-init/init.sql
@@ -20,20 +20,19 @@ CREATE TABLE preferences (
 -- Taula: repositories
 CREATE TABLE repositories (
     repo_id SERIAL PRIMARY KEY,
-    repo_season_id INT NOT NULL,
+    repo_season_id INT,
     repo_name VARCHAR(255) NOT NULL,
     repo_release_date DATE,
     repo_director VARCHAR(255),
     repo_cast TEXT,
     repo_description TEXT,
-    repo_is_series BOOLEAN NOT NULL
+    repo_is_series BOOLEAN NOT NULL,
     repo_image_path VARCHAR(255),
     repo_rating DECIMAL(3,1),
     repo_PEGI INT,
     repo_duration INT,
     repo_season_number INT,
-    repo_episode_count INT,
-    repo_genre JSON
+    repo_episode_count INT
 );
 
 -- Taula: library (relació user-repository amb dades)
@@ -42,7 +41,7 @@ CREATE TABLE library (
     user_id VARCHAR(12) REFERENCES users(user_id) ON DELETE CASCADE,
     repo_id INT REFERENCES repositories(repo_id) ON DELETE CASCADE,
     library_season_id INT,
-    library_rating DECIMAL(3,1),
+    library_rating DECIMAL(3,1) DEFAULT 0.0,
     library_comment VARCHAR(255)
 );
 
@@ -52,15 +51,29 @@ CREATE TABLE characters (
     repo_id INT REFERENCES repositories(repo_id) ON DELETE CASCADE,
     character_name VARCHAR(100) NOT NULL,
     character_context TEXT,
-    characters_image_path VARCHAR(255)
+    character_image_path VARCHAR(255)
 );
 
 -- Taula: questionnaires
 CREATE TABLE questionnaires (
     questionnaire_id SERIAL PRIMARY KEY,
     repo_id INT REFERENCES repositories(repo_id) ON DELETE CASCADE,
-    season_id INT,
+    repo_season_id INT,
     questionnaires_rating INT,
     questionnaires_comment VARCHAR(255),
     questionnaires_validated BOOLEAN DEFAULT FALSE
+);
+
+-- Taula de gèneres
+CREATE TABLE genres (
+    genre_id SERIAL PRIMARY KEY,
+    genre_name VARCHAR(50) UNIQUE NOT NULL
+);
+
+-- Taula relacional entre repositories i genres (N:M)
+CREATE TABLE repo_genres (
+    repo_id INT REFERENCES repositories(repo_id) ON DELETE CASCADE,
+    repo_season_id INT,
+    genre_id INT REFERENCES genres(genre_id) ON DELETE CASCADE,
+    PRIMARY KEY (repo_id, repo_season_id, genre_id)
 );
