4ccc76b72530ddc01f82ea8980b0ef469018bbd2
Joan <joan.corrals@autonoma.cat>
2025-05-06 09:51:22 +0200

    Es pot buscar per cast

--- backend/media/code/resources/media-get.js
+++ backend/media/code/resources/media-get.js
@@ -35,8 +35,7 @@ async function all(p, type, search, genre, director, cast, order) {
     if (cast) {
         filter = true;
         filterCount++;
-        filters.push("cast % '" + cast + "' ");
-    }
+        filters.push('"cast" ILIKE \'%' + cast + '%\'');    }
     for (let index = 0; index < filterCount; index++) {
         if (index == 0) {
             filterConcat = filterConcat + filters[index];
@@ -44,6 +43,7 @@ async function all(p, type, search, genre, director, cast, order) {
             filterConcat = filterConcat + "AND " + filters[index];
         }
     }
+    console.log(filters);
     console.log("p: ", p);
     console.log("type: ", type);
     let mult = (10 * (p - 1));
