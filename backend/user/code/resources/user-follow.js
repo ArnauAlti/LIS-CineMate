f660a97415c1a0ebf18bd4d8c243eb9774440802
Joan <joan.corrals@autonoma.cat>
2025-05-11 14:06:01 +0200

    Follow i unfollow fetes, falta comprovar funcionament

--- backend/user/code/resources/user-follow.js
+++ backend/user/code/resources/user-follow.js
@@ -4,6 +4,7 @@ async function follow(req, res) {
     try {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         let srcMail = req.body['srcMail'];
         let dstMail = req.body['dstMail'];
         if (!srcMail || !dstMail) {
@@ -19,13 +20,21 @@ async function follow(req, res) {
         let srcNick = req.body['srcMail'];
         let dstNick = req.body['dstMail'];
 >>>>>>> 9caeb69 (Començar a fer el follow)
+=======
+        let srcMail = req.body['srcMail'];
+        let dstMail = req.body['dstMail'];
+>>>>>>> aca64d9 (Follow i unfollow fetes, falta comprovar funcionament)
         if (!srcNick || !dstNick) {
             throw "Missing Information";
         } else {
             const query = await userDB.query(
                 'INSERT INTO following(src_mail, dst_mail) VALUES ($1, $2)',
+<<<<<<< HEAD
                 [srcNick, dstNick]
 >>>>>>> 88bba61 (Query de folling + Implementación de Follow)
+=======
+                [srcMail, dstMail]
+>>>>>>> aca64d9 (Follow i unfollow fetes, falta comprovar funcionament)
             );
             if (query.rowCount != 1) {
                 throw "Something Unexpected Happened During Insert";
