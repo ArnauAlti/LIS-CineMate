285e9468bdf9b15d3e141b7d521e20db2ff83614
Chescus <bycheca97@gmail.com>
2025-05-17 17:58:46 +0200

    character-chat -> canvis funcionalitat EVO 1

--- backend/character/code/resources/character-chat.js
+++ backend/character/code/resources/character-chat.js
@@ -11,13 +11,47 @@ async function chatCharacter(req, res) {
 
         const message = req.body['message'];
         console.log("message received:", message);
-
         if (!message) {
             throw "No message sent.";
         }
 
+        const title = req.body['movie_name'];
+        console.log(title);
+        if (!title) {
+            throw "No Title Sent";
+        } 
         //Aplicar lógica con el chatbot
-
+        const response = await axios.post("https://194.26.196.165:20651/api/init", {
+                "character_name": characterName,
+                "movie_title": title
+            },
+            {
+                headers: {
+                    'Content-Type': application/json
+                }
+            }
+        );
+        console.log(response);
+        if (response.status == 200) {
+            const patata = response.data.session_id;
+            const response2 = await axios.post("https://194.26.196.165:20651/api/chat", {
+                "question": message,
+                "session_id": patata,
+                "character_name": characterName,
+                "movie_title": title
+            },
+            {
+                headers: {
+                    'Content-Type': application/json
+                }
+            }
+        );
+            if (response2.status == 200) {
+                res.status(200).json({message: "OK", data: response2.data.response})
+            }
+        } else {
+            throw "First axios request failed";
+        }
     } catch (error) {
         console.error("Error in character chat:", error);
         res.status(500).json({ error: error, message: "Failed to receive messages" });
